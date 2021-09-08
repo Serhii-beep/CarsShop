99a8a4203593664d97de32618f65e78763fbaccf
Michael Bubka <bubkamisha14@gmail.com>
2021-09-08 17:21:40 +0300

    Add OrderRepository

---
+++ CarShop/Repository/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarShop.Models;
+namespace CarShop.Repository
+{
+    public interface IOrderRepository
+    {
+        void AddOrder(Order order);
+        Task<IEnumerable<Order>> GetOrders();
+        Order GetOrder(int id);
+        void DeleteOrder(int id);
+        void UpdateOrder(Order order);
+    }
+}
---
+++ CarShop/Repository/OrderRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace CarShop.Repository
+{
+    public class OrderRepository: IOrderRepository
+    {
+        DbCarShopContext _context;
+
+        public OrderRepository(DbCarShopContext context)
+        {
+            _context = context;
+        }
+
+        public async void AddOrder(Order order)
+        {
+            try
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Order GetOrder(int id)
+        {
+            Order order = _context.Orders.Where(a => a.OrderId == id).FirstOrDefault();
+            return order;
+        }
+
+        public async Task<IEnumerable<Order>> GetOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+
+        public async void UpdateOrder(Order order)
+        {
+
+            try
+            {
+                _context.Update(order);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException ex)
+            {
+                throw new DbUpdateConcurrencyException(ex.Message);
+            }
+        }
+
+        public async void DeleteOrder(int id)
+        {
+            Order order = await _context.Orders.FirstOrDefaultAsync(a => a.OrderId == id);
+            try
+            {
+                _context.Remove(order);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException ex)
+            {
+                throw new DbUpdateException(ex.Message);
+            }
+        }
+    }
+}
