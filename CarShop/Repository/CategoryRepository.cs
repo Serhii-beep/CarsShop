1fec40ee985b6c7499a1a9f2432146a8059783fb
Michael Bubka <bubkamisha14@gmail.com>
2021-09-08 16:14:44 +0300

    Add Category (I)Repository

---
+++ CarShop/Repository/CategoryRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarShop.Repository
+{
+    public class CategoryRepository: ICategoryRepository
+    {
+        DbCarShopContext _context;
+        public CategoryRepository(DbCarShopContext context)
+        {
+            _context = context;
+        }
+
+        public async void AddCategory(Category category)
+        {
+            try
+            {
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public Category GetCategory(int id)
+        {
+            Category category = _context.Categories.Where(a => a.CategoryId == id).FirstOrDefault();
+            return category;
+        }
+
+        public async void UpdateCategory(Category category)
+        {
+            try
+            {
+                _context.Update(category);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw new DbUpdateConcurrencyException(ex.Message);
+            }
+        }
+
+        public async void DeleteCategory(int id)
+        {
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.CategoryId == id);
+            try
+            {
+                _context.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw new DbUpdateConcurrencyException(ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ CarShop/Repository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop.Repository
+{
+    public interface ICategoryRepository
+    {
+        void AddCategory(Category category);
+        Task<IEnumerable<Category>> GetCategories();
+        Category GetCategory(int id);
+        void DeleteCategory(int id);
+        void UpdateCategory(Category category);
+    }
+}
