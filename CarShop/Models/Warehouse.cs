87f8dc6163362eb0806b35aa6a61033d38911c52
Michael Bubka <bubkamisha14@gmail.com>
2021-10-20 20:11:40 +0300

    add map

--- CarShop/Models/Warehouse.cs
+++ CarShop/Models/Warehouse.cs
@@ -8,6 +8,7 @@ namespace CarShop
 {
     public class Warehouse
     {
+        [Display (Name ="Warehouse")]
         public int WarehouseId { get; set; }
         [Required(ErrorMessage = "Required field")]
         public string Address { get; set; }
@@ -15,7 +16,7 @@ namespace CarShop
         [Range(-90, 90, ErrorMessage = "Latitude values ​​should be greater than 90 and less than 90")]
         public double Latitude { get; set; }
         [Required(ErrorMessage = "Required field")]
-        [Range(-90, 90, ErrorMessage = "Longitude values ​​should be greater than 0 and less than 360 ")]
+        [Range(0, 360, ErrorMessage = "Longitude values ​​should be greater than 0 and less than 360 ")]
         public double Longitude { get; set; }
 
         public virtual ICollection<Car> Cars { get; set; }
--- CarShop/wwwroot/js/map.js
+++ CarShop/wwwroot/js/map.js
@@ -1,14 +1,61 @@
-﻿
+﻿let map;
+let locations = [];
+let currentId;
+
+function initLocations(data) {
+    for (let i = 0; i < data.length; ++i) {
+        locations[i] = {};
+        locations[i].latitude = data[i].latitude;
+        locations[i].longitude = data[i].longitude;
+        locations[i].address = data[i].address;
+        locations[i].warehouseId = data[i].warehouseId;
+    }
+
+    var infowindow = new google.maps.InfoWindow();
+
+    const icons = {
+        current: {
+            icon: "/images/icons/icons8-marker-50.png",
+        }
+    };
+
+    currentId =  $('#warehouseId').attr("data-id");
+
+    for (let i = 0; i < locations.length; i++) {
+        if (locations[i].warehouseId == currentId) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
+                icon: icons["current"].icon,
+                map: map,
+            });
+        }
+        else {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
+                map: map,
+            });
+        }
+
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+                infowindow.setContent(locations[i].warehouseId == currentId ? "(Current warehouse) " + locations[i].address: locations[i].address);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+}
 function initMap() {
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: new google.maps.LatLng(50.450001, 30.523333),
+    
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 50.450001, lng: 30.523333 },
+        zoom: 4,
     });
+   
+    fetch("https://localhost:44326/api/WarehousesApi").then(response => response.json()).then(data => initLocations(data));
 
 }
 
-
-
-
-
+function setId(Id) {
+    currentId = Id;
+}
